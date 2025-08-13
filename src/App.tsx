87c72f89e7dc2a7a70d5d04488cb2cf02baf2a38
Hadasa Matan <hadasamatan@gmail.com>
2025-08-13 18:35:39 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import SurveyForm from './components/SurveyForm';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MainLayout from './components/MainLayout';
 
 function App() {
-  return <SurveyForm />;
+  return (
+    <Router basename="/form-4u">
+      <MainLayout />
+    </Router>
+  );
 }
 
 export default App;
